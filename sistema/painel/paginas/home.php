b98613d610a7729c5bbb08ef5edd9b1a6648b0c9
jonasfp <jfp@discente.ifpe.edu.br>
2022-08-24 15:09:33 -0300

    Criação de tela de login e tela home

---
+++ sistema/painel/paginas/home.php
@@ -0,0 +1,103 @@
+
+	<div class="main-page">
+		<div class="col_3">
+			<div class="col-md-3 widget widget1">
+				<div class="r3_counter_box">
+					<i class="pull-left fa fa-dollar icon-rounded"></i>
+					<div class="stats">
+						<h5><strong>$452</strong></h5>
+						<span>Total Revenue</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 widget widget1">
+				<div class="r3_counter_box">
+					<i class="pull-left fa fa-laptop user1 icon-rounded"></i>
+					<div class="stats">
+						<h5><strong>$1019</strong></h5>
+						<span>Online Revenue</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 widget widget1">
+				<div class="r3_counter_box">
+					<i class="pull-left fa fa-money user2 icon-rounded"></i>
+					<div class="stats">
+						<h5><strong>$1012</strong></h5>
+						<span>Expenses</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 widget widget1">
+				<div class="r3_counter_box">
+					<i class="pull-left fa fa-pie-chart dollar1 icon-rounded"></i>
+					<div class="stats">
+						<h5><strong>$450</strong></h5>
+						<span>Expenditure</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 widget">
+				<div class="r3_counter_box">
+					<i class="pull-left fa fa-users dollar2 icon-rounded"></i>
+					<div class="stats">
+						<h5><strong>1450</strong></h5>
+						<span>Total Users</span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+
+		<div class="row-one widgettable">
+
+			<div class="col-md-8 content-top-2 card">
+
+
+
+			</div>
+
+			<div class="col-md-4 stat">
+				<div class="content-top-1">
+					<div class="col-md-6 top-content">
+						<h5>Sales</h5>
+						<label>1283+</label>
+					</div>
+					<div class="col-md-6 top-content1">	   
+						<div id="demo-pie-1" class="pie-title-center" data-percent="45"> <span class="pie-value"></span> </div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="content-top-1">
+					<div class="col-md-6 top-content">
+						<h5>Reviews</h5>
+						<label>2262+</label>
+					</div>
+					<div class="col-md-6 top-content1">	   
+						<div id="demo-pie-2" class="pie-title-center" data-percent="75"> <span class="pie-value"></span> </div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="content-top-1">
+					<div class="col-md-6 top-content">
+						<h5>Visitors</h5>
+						<label>12589+</label>
+					</div>
+					<div class="col-md-6 top-content1">	   
+						<div id="demo-pie-3" class="pie-title-center" data-percent="90"> <span class="pie-value"></span> </div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+
+			<div class="col-md-2 stat"> </div>
+
+			<div class="clearfix"> </div>
+
+		</div>
+
+		<div class="charts"> </div>		
+		
+		
+
+	</div>
